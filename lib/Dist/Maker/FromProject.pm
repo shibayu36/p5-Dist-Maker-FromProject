8498303ee183b07b58db549df77bbebcd3561ca3
shiba_yu36 <shibayu36@gmail.com>
2013-05-27 20:51:11 +0900

    dim-from-project

--- lib/Dist/Maker/FromProject.pm
+++ lib/Dist/Maker/FromProject.pm
@@ -5,6 +5,83 @@ use warnings;
 
 our $VERSION = '0.01';
 
+use Config::PL;
+use Path::Tiny;
+use Class::Accessor::Lite::Lazy (
+    new     => 1,
+    ro      => [qw(class_name project_dir config_file)],
+    ro_lazy => [qw(project_files template_format config)],
+);
+
+sub _build_project_files {
+    my ($self) = @_;
+    my $project_dir = $self->project_dir;
+
+    my $out = qx{cd $project_dir && git ls-files};
+    my $project_files = [ split "\n", $out ];
+
+    return $project_files;
+}
+
+sub _build_template_format {
+    my ($self) = @_;
+    my $format = "package Dist::Maker::Template::@{[ $self->class_name ]};\n";
+    $format .= <<'FORMAT';
+use utf8;
+use Mouse;
+use MouseX::StrictConstructor;
+
+use Dist::Maker::Util qw(run_command);
+
+extends 'Dist::Maker::Base';
+with    'Dist::Maker::Template';
+
+sub dist_init {
+}
+
+sub distribution {
+    # empty expression <: :> is used
+    # in order to avoid to confuse PAUSE indexers
+    return <<'DIST';
+%s
+DIST
+}
+
+no Mouse;
+__PACKAGE__->meta->make_immutable();
+FORMAT
+}
+
+sub _build_config {
+    my ($self) = @_;
+    return config_do $self->config_file;
+}
+
+sub make_template {
+    my ($self) = @_;
+
+    my $project_files = $self->project_files;
+    my $dist_content = '';
+    for my $filename (@$project_files) {
+        $dist_content .= $self->_make_dist_content_from_file($filename);
+    }
+
+    return sprintf($self->template_format, $dist_content);
+}
+
+sub _make_dist_content_from_file {
+    my ($self, $filename) = @_;
+    my $config = $self->config;
+    my $content = path($self->project_dir)->child($filename)->slurp;
+
+    for my $replace_from (keys %$config) {
+        my $replace_to = $config->{$replace_from};
+        $filename =~ s{\Q$replace_from\E}{$replace_to}ge;
+        $content =~ s{\Q$replace_from\E}{$replace_to}ge;
+    }
+
+    return sprintf("@@ %s\n%s\n", $filename, $content);
+}
 
 1;
 __END__
---
+++ lib/Dist/Maker/FromProject/CLI.pm
@@ -0,0 +1,61 @@
+package Dist::Maker::FromProject::CLI;
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Dist::Maker::FromProject;
+
+sub new {
+    my $class = shift;
+    bless { }, $class;
+}
+
+sub run {
+    my ($self, @args) = @_;
+
+    local @ARGV = @args;
+    my $p = Getopt::Long::Parser->new(
+        config => ["no_ignore_case", "pass_through"],
+    );
+    $p->getoptions(
+        "help=s"        => \$self->{help},
+        "class=s"       => \$self->{class},
+        "config-file=s" => \$self->{config_file},
+        "project-dir=s" => \$self->{project_dir},
+    );
+
+    # --help option
+    if ($self->{help}) {
+        $self->usage;
+        return;
+    }
+
+    unless ($self->{class} && $self->{config_file} && $self->{project_dir}) {
+        $self->usage;
+        return;
+    }
+
+    my $dim_from_project = Dist::Maker::FromProject->new({
+        class_name  => $self->{class},
+        project_dir => $self->{project_dir},
+        config_file => $self->{config_file},
+    });
+    my $template = $dim_from_project->make_template;
+
+    $self->print($template);
+}
+
+sub usage {
+    my $self = shift;
+    my $msg = <<"HELP";
+Usage: dim-from-project --class=WebApp --config-file=<path> --project-dir=<path> [--help]
+HELP
+    $self->print($msg);
+}
+
+sub print {
+    my ($self, $msg) = @_;
+    print STDERR $msg;
+}
+
+1;
